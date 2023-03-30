77a2db737fb9871152b3b4657df6d856711751bb
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2023-03-30 13:38:01 -0500

    mod

--- modulo_usuario/dashboard.php
+++ modulo_usuario/dashboard.php
@@ -5,22 +5,60 @@
 	$userq=mysqli_query($connect,"select * from `tb_usuario` where id='$userid'");
 	$userrow=mysqli_fetch_array($userq);
 ?>
-
+<!-- HTML codigo -->
 <!doctype html>
 <html>
     <meta charset="UTF-8" />
-    <link rel="stylesheet" type="text/css" href="css/panel_principal.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../css/dashboardUsuario.css">
     <head>
+        <script type ="text/javascript" src="../js/sideBar.js"></script>
         <title>Mis Cursos - Sistema de Estimacion de Tendnecias / Inadeh</title>
     </head>
     <body>
+        <!-- Boton main bar 
+        <div id="main">
+          <button class="openbtn" onclick="openNav()">☰ Open Sidebar</button> 
+        </div> -->
+        <!-- Side Bar -->
+        <div class="topnav">
+        <button class="openbtn" onclick="openNav()">☰</button> 
+        <a class="openbtn" href="#" onclick="openNav()">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+        <div class="search-container">
+            <form action="/action_page.php">
+            <input type="text" placeholder="Search.." name="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+        </div>
+        </div>
+        <div id="mySidebar" class="sidebar">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+        <a >Hola, <?php echo $userrow['nombre'],$userrow['apellido']; ?> </a><br>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+        </div>
+
+        <!-- Boton main bar -->
         <h2>Bienvenido </h2>
         Hola, <?php echo $userrow['nombre'],$userrow['apellido']; ?><br>
         <a href='../seguridad/salir.php'><button type='button'>Salir</button></a>
+        
+  
+       
 
         <!-- Portfolio Gallery Grid -->
-
-  
+        <nav>    
+        <div class="card-list">
+            <div class="wat">
+                <h1>Cursos Inscritos</h1>
+            </div>
+               
+             <div class="cards">
+        
   <?php
         //$sql = "SELECT * FROM tb_cursos WHERE status='por confirmar' OR status='pendiente' OR status='confirmado' OR status='Realizado'";
             $sql = "SELECT * FROM tb_misCursos where id_usuario = $userid ";
@@ -28,26 +66,28 @@
             if($result->num_rows > 0) {
                 while($row = $result->fetch_assoc()) 
                 {
-                echo "<div class='column'>
-                      <div class='content'>
-                        <h5>".$row['nombre_curso']."</h5>
+                echo "<div class='card'>
+                        <h3>".$row['nombre_curso']."</h3>
                         <h5>".$row['codigo_curso']."</h5>
                           <div class='btn-cursos'>
                             <a href='borrarCurso.php?idMisCursos=".$row['id']."'><button type='button'>X</button></a>
                           </div>
-
-                      </div>
-                      </div> ";
+                     </div>
+                     ";
                   }
                   } else {
                       echo "<tr><td colspan='5'><center>No estas inscrito a ningun curso!</center></td></tr>";
                   }
               ?>
-    <section>Cursos Disponibles</section>
+</div> 
+</div> 
+</div> 
+</nav>
+ 
 
 <!-- Cursos Disponible -->
 <div class="row">
-  
+    <h3>Cursos Disponibles</h3>
       <?php
             //$sql = "SELECT * FROM tb_cursos WHERE status='por confirmar' OR status='pendiente' OR status='confirmado' OR status='Realizado'";
                 $sql1 = "SELECT * FROM tb_cursos ";
@@ -56,8 +96,8 @@
                     while($row = $result1->fetch_assoc()) 
                     {
                     echo "<div class='column'>
-                          <div class='content'>
-                            <img src='../img/".$row['foto']." 'style='width:10%'>
+                            <div class='content'>
+                            <img src='../img/".$row['foto']." 'style='width:40%'>
                             <h5>".$row['nombre']."</h5>
                             <p>".$row['codigo']."</p><p>Horas: ".$row['horas']."</p>
                               <div class='btn-cursos'>
@@ -65,22 +105,24 @@
                               </div>
 
                               <div class='btn-adwishlist'>
-                              <a href='accionInscripcion.php?idCursos=".$row['id']."'><button type='submit' name='paso1'>Inscribirse</button></a>
+                                <a href='accionInscripcion.php?idCursos=".$row['id']."'><button type='submit' name='paso1'>Inscribirse</button></a>
                               </div>
 
                               <div class='btn-adwishlist'>
-                              <button>Añadir a Lista de Deseos</button>
+                                <button>Añadir a Lista de Deseos</button>
                               </div>
-
-                          </div>
+                             
+                              </div> 
                           </div> ";
                       }
                       } else {
                           echo "<tr><td colspan='5'><center>!</center></td></tr>";
                       }
                   ?>
-</div>
-
+            </div>
+            </div> 
+</div> 
+</div> 
     </body>
 
 
